4af8f225c3235d4ec08706188a5765b732275d86
Bing <huangjianbing@outlook.com>
2022-12-02 17:25:44 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-html_logo = 'logo.png'
+html_logo = 'logoHome.png'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
